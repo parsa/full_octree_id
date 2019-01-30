8ea3cb4953264063871584831c8dedb2f713f1f8
Parsa Amini <parsa@cct.lsu.edu>
2019-01-30 12:24:19 -0600

    Port to Python

---
+++ full_octree_id.py
@@ -0,0 +1,62 @@
+import argparse
+import sys
+
+NDIM = 3
+
+def to_id(x, lev):
+    id = 1
+    for l in range(0, lev):
+        for d in range(0, NDIM):
+            id <<= 1
+            id |= (x[d] >> l) & 1
+    return id
+
+def from_id(id):
+    x = [0] * NDIM
+    lev = 0
+    
+    while id != 1:
+        print('{} {:o}'.format(lev, id))
+        for d in range(NDIM - 1, -1, -1):
+            x[d] <<= 1
+            x[d] |= (id & 1)
+            id >>= 1
+        lev += 1
+        
+    return x, lev
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Generate the Octree for sub-elements of a Octree')
+    parser.add_argument('base', type=str)
+    parser.add_argument('dlev', type=int)
+    args = parser.parse_args()
+
+    ptr = args.base
+    dlev = args.dlev
+    assert(ptr[0] == '1')
+    base_id = 1
+    ptr = ptr[1:]
+
+    while ptr:
+        base_id <<= NDIM
+        base_id |= (ord(ptr[0]) - ord('0'))
+        ptr = ptr[1:]
+
+    y = [0] * NDIM
+
+    x, lev = from_id(base_id)
+
+    print("base_level is", lev)
+
+    for i in range(0, 1 << dlev):
+        for j in range(0, 1 << dlev):
+            for k in range(0, 1 << dlev):
+                y[0] = (x[0] << dlev) + i
+                y[1] = (x[1] << dlev) + j
+                y[2] = (x[2] << dlev) + k
+                new_id = to_id(y, lev + dlev)
+                print('the ({},{},{}) cell in subgrid {} has full octree id {:o}'.format(i, j, k, args.base, new_id))
+
+if __name__ == '__main__':
+    main()
